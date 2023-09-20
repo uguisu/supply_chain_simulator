7f45b58b029b0a5c7531a790dd68ee6bcb57a7d0
uguisu <uguisu@users.noreply.github.com>
2023-09-20 09:54:02 +0800

    udpate: new test function

---
+++ src/scs/core/scs_main.cpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2023
+// All rights reserved.
+//
+// Author: xin he
+//
+#ifndef SCS_MAIN_CPP
+#define SCS_MAIN_CPP
+
+#include <iostream>
+
+#include "scs_main.hpp"
+
+namespace scs { namespace core {
+
+void test_hello(const std::string &name)
+{
+    std::cout << "hello " << name;
+}
+
+}}
+
+#endif /* SCS_MAIN_CPP */
---
+++ src/scs/core/scs_main.hpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2023
+// All rights reserved.
+//
+// Author: xin he
+//
+#ifndef SCS_MAIN_HPP
+#define SCS_MAIN_HPP
+
+#include <string>
+
+namespace scs { namespace core {
+
+void test_hello(const std::string &name);
+
+}}
+
+#endif /* SCS_MAIN_HPP */
