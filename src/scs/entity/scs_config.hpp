a87b743805a9c1f0bc807a453788ad5b4f4fc4b3
uguisu <uguisu@users.noreply.github.com>
2023-09-22 00:29:22 +0800

    update: json related entity

--- src/scs/entity/scs_config.hpp
+++ src/scs/entity/scs_config.hpp
@@ -7,26 +7,135 @@
 #define SCS_CONFIG_HPP
 
 #include <string>
+#include <vector>
 
 namespace scs { namespace entity {
+
+// pre-declare class
+class ScsConfig;
+// For: "lo", "ls"
+class ScsConfigLoLs;
+// For: "formula"
+class ScsConfigFormula;
+// For: "edges"
+class ScsConfigEdge;
+// For: "holding_cost", "shortage_cost", "consumer"
+class ScsConfigCost;
+// TODO 残值成本函数
+
+// For: "func"
+class ScsConfigFunc;
+// For: "manufacture"
+class ScsConfigManufacture;
+// For: "component"
+class ScsConfigComponent;
+
+
+class ScsConfigFunc
+{
+    public:
+        /** item id */
+        std::string itemId;
+        /** function id */
+        std::string funcId;
+
+        ~ScsConfigFunc() {};
+};
+
+class ScsConfigCost
+{
+    public:
+        /** node id */
+        std::string nodeId;
+        /** function list */
+        std::vector<ScsConfigFunc> funcList;
+
+        ~ScsConfigCost() {};
+};
+
+class ScsConfigEdge
+{
+    public:
+        /** path */
+        std::string path;
+        /** item list */
+        std::vector<std::string> items;
+
+        ~ScsConfigEdge() {};
+};
+
+class ScsConfigComponent
+{
+    public:
+        /** item id */
+        std::string itemId;
+        /** materials */
+        float materials;
+
+        ~ScsConfigComponent() {};
+};
+
+class ScsConfigManufacture
+{
+    public:
+        /** item id */
+        std::string itemId;
+        /** process time */
+        float processTime;
+        /** component list */
+        std::vector<ScsConfigComponent> componentList;
+
+        ~ScsConfigManufacture() {};
+};
+
+class ScsConfigFormula
+{
+    public:
+        /** node id */
+        std::string nodeId;
+        /** manufacture */
+        std::vector<ScsConfigManufacture> manufacture;
+
+        ~ScsConfigFormula() {};
+};
+
+class ScsConfigLoLs
+{
+    public:
+        /** path */
+        std::string path;
+        /** function list */
+        std::vector<ScsConfigFunc> funcList;
+
+        ~ScsConfigLoLs() {};
+};
 
 class ScsConfig
 {
     public:
-        /**
-         * setNodeAmount
-         * @param node_amount total node amount
-         */
-        void setNodeAmount(const int16_t &node_amount);
+
+        /** lo */
+        std::vector<ScsConfigLoLs> lo;
+        /** ls */
+        std::vector<ScsConfigLoLs> ls;
+        /** formula */
+        std::vector<ScsConfigFormula> formula;
+        /** edges */
+        std::vector<ScsConfigEdge> edges;
+        /** holding cost */
+        std::vector<ScsConfigCost> holdingCost;
+        /** shortage cost */
+        std::vector<ScsConfigCost> shortageCost;
+        /** consumer */
+        std::vector<ScsConfigCost> consumer;
+        // TODO 残值成本函数
+
         /**
          * getNodeAmount
          */
         int16_t getNodeAmount();
 
         ~ScsConfig();
-    private:
-        /** node_amount */
-        int16_t node_amount = 1;
 };
 
 }}
