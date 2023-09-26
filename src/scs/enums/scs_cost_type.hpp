5c30dda9ba5658a2f155a1733367fe11df57dc85
uguisu <uguisu@users.noreply.github.com>
2023-09-26 08:55:09 +0800

    update: invite CostType

---
+++ src/scs/enums/scs_cost_type.hpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2023
+// All rights reserved.
+//
+// Author: xin he
+//
+#ifndef SCS_COST_TYPE_HPP
+#define SCS_COST_TYPE_HPP
+
+#include <string>
+
+namespace scs { namespace enums {
+
+/**
+ * cost type
+ */
+enum class CostType
+{
+    HoldingCost = 1,
+    ShortageCost = 2,
+    SalvageCost = 3,
+    Consumer = 4
+};
+
+}}
+
+#endif /* SCS_COST_TYPE_HPP */
