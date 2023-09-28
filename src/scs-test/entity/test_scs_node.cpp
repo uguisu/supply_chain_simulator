1ee44ec3b80116100bd629bf78c1b5d0be9cfef2
uguisu <uguisu@users.noreply.github.com>
2023-09-28 15:57:34 +0800

    update: new test case

--- src/scs-test/entity/test_scs_node.cpp
+++ src/scs-test/entity/test_scs_node.cpp
@@ -20,10 +20,43 @@ void test_all()
 {
     LOG(INFO) << "Test -> test_scs_node start";
 
+    test_node_001();
+    test_edge_001();
+    
     test_901();
     test_902();
 
     LOG(INFO) << "Test -> test_scs_node end";
+}
+
+void generateEdge(scs::entity::ScsEdge &edge)
+{
+    edge.id = EDGE_ID;
+    edge.fromNodeId = FROM_ID;
+    edge.toNodeId = TO_ID;
+    edge.itemId = ITEM_ID;
+    edge.lsFuncId = FUNC_ID_1;
+    edge.loFuncId = FUNC_ID_2;
+}
+
+void test_node_001()
+{
+    scs::entity::ScsNode node;
+    node.id = ID;
+    assert(ID == node.id);
+}
+
+void test_edge_001()
+{
+    scs::entity::ScsEdge edge;
+    generateEdge(edge);
+
+    assert(EDGE_ID == edge.id);
+    assert(FROM_ID == edge.fromNodeId);
+    assert(TO_ID == edge.toNodeId);
+    assert(ITEM_ID == edge.itemId);
+    assert(FUNC_ID_1 == edge.lsFuncId);
+    assert(FUNC_ID_2 == edge.loFuncId);
 }
 
 void test_901()
