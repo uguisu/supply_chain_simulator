e353efaf0a13775b6ac42d8a50f4e9e7c4e63047
uguisu <uguisu@users.noreply.github.com>
2023-10-09 16:24:58 +0800

    update: build to python lib work, but still have problem

---
+++ py_wrapper_src/scs/setup.py
@@ -0,0 +1,69 @@
+from distutils.core import setup, Extension
+from pathlib import Path
+
+
+# === base info ===
+model_name = 'scs'
+src_dirs = [
+    '../../src/scs/core',
+    '../../src/scs/entity',
+    '../../src/scs/enums',
+    '../../src/scs/function',
+]
+
+
+def get_sources() -> list:
+
+    rtn = []
+
+    for strP in src_dirs:
+        path = Path(strP).resolve()
+        for fi in path.glob('*.cpp'):
+            rtn.append(f'{path}/{fi.name}')
+
+    return rtn
+
+
+def get_include_dir() -> list:
+
+    return [
+        '/usr/local/include',
+    ]
+
+
+def get_libraries() -> list:
+    return [
+        'python3.8',
+        'glog',
+        'gflags',
+    ]
+
+
+def get_runtime_library_dirs() -> list:
+    return [
+        '/usr/local/lib',
+    ]
+
+
+def get_extension() -> Extension:
+
+    return Extension(
+        name = model_name,
+        sources = get_sources(),
+        include_dirs = get_include_dir(),
+        libraries=get_libraries(),
+        runtime_library_dirs=get_runtime_library_dirs(),
+    )
+
+
+def main():
+    setup(name=model_name,
+          version="1.0.0",
+          description="scs test",
+          author="xin.he",
+          author_email="xin.he@123.com",
+          ext_modules=[get_extension()])
+
+
+if __name__ == "__main__":
+    main()
